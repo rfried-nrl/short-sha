3c2d31fb7782c835abd5adfe6f3fafaa6a40e1a1
rfried-nrl <ramon@neureality.ai>
2021-04-28 08:35:24 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import * as core from '@actions/core'
-import {context} from '@actions/github'
 import {shorten} from './shorten'
 
 async function run(): Promise<void> {
   try {
     const sha = core.getInput('sha1')
     core.debug(`Sha:    ${sha}`)
-    const length = Number()
+    const length = Number(core.getInput('length'))
     core.debug(`Length: ${length}`)
     const shortSha = shorten(sha, length)
     core.debug(`Output: ${shortSha}`)
